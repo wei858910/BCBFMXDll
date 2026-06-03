04b7e099642e250bf023f890459fa76f5a1f00d8
Weiwei <93496699+wei858910@users.noreply.github.com>
2026-06-03 18:02:16 +0800

    基本框架

--- File1.cpp
+++ File1.cpp
@@ -1,10 +1,33 @@
-#include <fmx.h>
+﻿#include <fmx.h>
+#include <windows.h>
+#include "Unit1.h"
 
 #pragma hdrstop
 #pragma argsused
 
-extern "C" int _libmain(unsigned long reason)
+void WINAPI Run()
 {
-	return 1;
+    Application->Initialize();
+    Application->CreateForm(__classid(TForm1), &Form1);
+    Application->Run();
+
+    FreeLibraryAndExitThread(HInstance, 0);
 }
 
+DWORD WINAPI DllCallback(UIntPtr dwReason)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(HInstance);
+        DWORD tid{};
+        CreateThread(nullptr, 0, LPTHREAD_START_ROUTINE(Run), nullptr, 0, nullptr);
+    }
+
+    return 0;
+}
+
+extern "C" int _libmain(unsigned long reason)
+{
+    DllCallback(reason);
+    return 1;
+}
